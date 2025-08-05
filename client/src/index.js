5214065f2e6bab740149747cbf766cc3a05d21f4
tesfu3k <tesfaye4r@gmail.com>
2025-08-05 10:08:16 +0300

    doc: feedback on the client

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {Toaster } from "react-hot-toast"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { Toaster } from "react-hot-toast";
 
+/*
+ when you want to create react app instead of using create-react-app
+ you should use vite to make everting fast and modern
+ and vite a tool for the front end
+ for more read this: https://vite.dev/
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+*/
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
     <Toaster />
   </React.StrictMode>
 );
-
